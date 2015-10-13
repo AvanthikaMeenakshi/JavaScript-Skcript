d95dc253ca55a3d36619fe565b705c363dc4d5ac
AvanthikaMeenakshi <avanthika@skcript.com>
2015-10-13 12:14:44 +0530

    new Files

--- Scrpt1.js
+++ Scrpt1.js
@@ -2,17 +2,19 @@ $("#myBtn").click( function(event)
 {
 		event.preventDefault();
 		var city = $('#someText').val();
-		var table = $('#thisTable');
+		var table= $('#thisTable');
 		$.ajax({
 			method: "POST",
-			url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric",
+			url: "http://api.openweathermap.org/data/2.5/find?q="+city+"&units=metric&appid=bd82977b86bf27fb59a04b61b657fb6f",
 			dataType: "JSON",
 			success: function(data){
-				temp = data["main"]["temp"]
-				table.append('<tr><td>'+city+'</td><td>'+temp+'</td></tr>')
+				temp = data[main][temp];
+				obj = $.parseJSON(temp)
+				table.append('<tr><td>'+city+'</td><td>'+temp+'</td></tr>');
 			}, 
 			error: function(data){
 				alert("404 not found");
 			}
 		});
+		
 });
