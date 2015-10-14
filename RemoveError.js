3856379e1766aca955a775809fc84deb15f1c741
AvanthikaMeenakshi <avanthika@skcript.com>
2015-10-14 18:08:37 +0530

    Activity 1 to 5

---
+++ RemoveError.js
@@ -0,0 +1,6 @@
+function removeP()
+{
+	document.getElementById("nameerrormsg").style.visibility = 'hidden' ;
+	document.getElementById("addrerrormsg").style.visibility = 'hidden' ;
+	document.getElementById("posterrormsg").style.visibility = 'hidden' ;
+}
--- checkForm.js
+++ checkForm.js
@@ -1,24 +1,39 @@
-function checkThis(){
+function checkThis(event){
 	var nameText = document.getElementById("fullname");
-	if (nameText.value == 0)
+	var addText = document.getElementById("streetaddr");
+	var pocode = document.getElementById("postcode");
+	var mail = document.getElementById("email").value;
+	var atpos = mail.indexOf("@");
+    var dotpos = mail.lastIndexOf(".");
+    
+	if (nameText.value == "")
 	{
 		document.getElementById("nameerrormsg").style.visibility = 'visible' ;
 		nameText.focus();
+		event.preventDefault();
 	}
-	var addText = document.getElementById("streetaddr");
-	if (addText.value == 0) 
-	{
+	
+	if (addText.value == "") 
+	{	
 		document.getElementById("addrerrormsg").style.visibility='visible';
 		addText.focus();
-		return false;
+		event.preventDefault();
 	}
-	var quantity = document.getElementById("quantity");
-	if (isNaN(quantity.value))
-  	{
-    	window.alert("Enter valid number");
-    	return false;
-  	}
-  	else{
-		window.alert("Submitted!");
-	}
+
+  	if (pocode.value == 0) 
+  		{
+  			document.getElementById("posterrormsg").style.visibility='visible';
+			pocode.focus();
+			event.preventDefault();
+  		}
+
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=mail.length) {
+        alert("Not a valid e-mail address");
+        event.preventDefault();
+    }
+
+  	else
+  		{
+  			window.alert("Form submitted");
+  		}
 }
